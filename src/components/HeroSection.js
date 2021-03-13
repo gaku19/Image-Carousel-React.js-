36d49ebf773cb76439922c3117ae17b7ce7aa03d
Gaku Ezaki <ezaki.gaku@gmail.com>
2021-03-13 16:04:32 +0900

    minor changes

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -4,8 +4,7 @@ import './HeroSection.css'
 function HeroSection() {
     return (
         <div className="hero-container">
-            <h1>Kawahara Group</h1>
-            <p>シャンデリア東京</p>
+            
         </div>
     )
 }
